4c582ae991b3ca5737e507048ee88f95aef7e6ef
Suraj Thapa <suraj.thapa@yipl.com.np>
2023-01-24 01:49:09 +0545

    Banner page design

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,5 @@
 const AppConfig = {
-  MAINTENANCE_MODE: Boolean(process.env.NEXT_APP_MAINTENANCE_MODE || false),
+  MAINTENANCE_MODE: Boolean( false),
   MAINTENANCE_MODE_MESSAGE:
     process.env.NEXT_APP_MAINTENANCE_MODE_MESSAGE || 'Site under construction!',
   GITHUB_URL:process.env.NEXT_APP_GITHUB_URL || 'https://github.com/SurajMazar/video-resume'
